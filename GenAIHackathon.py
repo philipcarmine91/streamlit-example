dee8475beb3ea528b9ccb3ded5b09a9fac2d8645
philipcarmine91 <129028846+philipcarmine91@users.noreply.github.com>
2023-10-20 13:30:22 +1100

    fix error

--- GenAIHackathon.py
+++ GenAIHackathon.py
@@ -2,16 +2,19 @@ import streamlit as st
 
 # Define the function for each page
 def build_cv_page():
-    st.header("Build your CV")
+    st.title("Build your CV")
     # Add content for the "Build your CV" page here
+    st.write("This is the content for 'Build your CV' page.")
 
 def build_cover_letter_and_interview_page():
-    st.header("Build a Cover Letter and Prepare for Interviews")
+    st.title("Build a Cover Letter and Prepare for Interviews")
     # Add content for the "Build a Cover Letter and Prepare for Interviews" page here
+    st.write("This is the content for 'Build a Cover Letter and Prepare for Interviews' page.")
 
 def explore_career_options_page():
-    st.header("Explore your Career Options")
+    st.title("Explore your Career Options")
     # Add content for the "Explore your Career Options" page here
+    st.write("This is the content for 'Explore your Career Options' page.")
 
 # Create a dictionary to map page names to functions
 page_dict = {
@@ -20,19 +23,13 @@ page_dict = {
     "Explore your Career Options": explore_career_options_page,
 }
 
-# Create a sidebar with image buttons for navigation
-with st.sidebar:
-    st.header("Navigation")
+# Create a sidebar with images as buttons for navigation
+st.sidebar.image("cv_image.png", use_column_width=True, output_format="PNG")
+st.sidebar.image("cover_letter_image.png", use_column_width=True, output_format="PNG")
+st.sidebar.image("career_options_image.png", use_column_width=True, output_format="PNG")
 
-    # Use st.image inside the button to show images as buttons
-    if st.button("Build your CV", key="cv_button"):
-        page_dict["Build your CV"]()
-    st.image("cv_image.png", use_column_width=True)
+# Get the user's choice from the sidebar
+selected_page = st.sidebar.selectbox("Select a Page", list(page_dict.keys()))
 
-    if st.button("Build a Cover Letter and Prepare for Interviews", key="cover_letter_button"):
-        page_dict["Build a Cover Letter and Prepare for Interviews"]()
-    st.image("cover_letter_image.png", use_column_width=True)
-
-    if st.button("Explore your Career Options", key="career_options_button"):
-        page_dict["Explore your Career Options"]()
-    st.image("career_options_image.png", use_column_width=True)
+# Call the corresponding function based on the user's choice
+page_dict[selected_page]()
